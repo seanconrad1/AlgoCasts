e9094069fbd6f875ad8c950cf8cb4032995f8b9c
seanrad <seanconrad123@gmail.com>
2018-12-10 15:00:14 -0500

    finished vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,10 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi)
+  return matches ? matches.length : 0
+}
 
+console.log(vowels('Why do you ask?'))
 module.exports = vowels;
